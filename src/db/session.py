cd38f6d43c3a91a0505c3cf220ef812084d04f42
Thai Nguyen <thai@ctvdsongkhoe.org>
2026-06-28 12:25:47 -0700

    feat(01-01): set up SQLAlchemy engine, session factory, and model registry

---
+++ src/db/session.py
@@ -0,0 +1,9 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./mediot.db")
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
---
+++ src/models/__init__.py
@@ -0,0 +1,5 @@
+from src.models.user import User
+from src.models.device import Device
+from src.models.vitals import Vitals
+
+__all__ = ["User", "Device", "Vitals"]

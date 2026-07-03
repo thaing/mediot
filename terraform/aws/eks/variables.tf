5ac3cc8163e9ae1c5ccce15db1567d763f202d94
Thai Nguyen <thai@ctvdsongkhoe.org>
2026-07-03 14:29:47 -0700

    chore: reduce replicas and nodes from 2→1 to cut costs

--- terraform/aws/eks/variables.tf
+++ terraform/aws/eks/variables.tf
@@ -1,4 +1,4 @@
 variable "region" { default = "us-east-1" }
 variable "cluster_name" { default = "mediot-cluster" }
 variable "node_instance_type" { default = "t3.medium" }
-variable "node_count" { default = 2 }
+variable "node_count" { default = 1 }
